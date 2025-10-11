e421816ef05f50a97691e167e87952623d6a5ca7
Chris Hopkin <christopher.hopkin@gmail.com>
2025-10-11 14:33:13 +0100

    Add the ComposeInfoWindowContentAdapter

---
+++ maps-compose/src/main/java/com/google/maps/android/compose/ComposeInfoWindowContentAdapter.kt
@@ -0,0 +1,29 @@
+package com.google.maps.android.compose
+
+import android.view.View
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionContext
+import androidx.compose.ui.platform.ComposeView
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.Marker
+
+public class ComposeInfoWindowContentAdapter(
+    private val mapView: MapView,
+    private val markerInfoContent: @Composable (Marker) -> Unit,
+    private val hasMarkerInfoContent: (Marker) -> Boolean = { true },
+    private val compositionContext: CompositionContext,
+) : GoogleMap.InfoWindowAdapter {
+
+    override fun getInfoContents(marker: Marker): View? {
+        if (!hasMarkerInfoContent(marker)) return null
+
+        val view = ComposeView(mapView.context).apply {
+            setContent { markerInfoContent(marker) }
+        }
+        mapView.renderComposeViewOnce(view, parentContext = compositionContext)
+        return view
+    }
+
+    override fun getInfoWindow(marker: Marker): View? = null
+}
